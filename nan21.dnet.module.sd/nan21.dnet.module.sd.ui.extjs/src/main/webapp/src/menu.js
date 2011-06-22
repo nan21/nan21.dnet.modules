2f879dbbf367c3b52e9d7a7d56a23a545ee4b7b3
attila.mathe <mathe_attila@yahoo.com>
2011-06-22 18:42:48 +0300

    sd - menus

---
+++ nan21.dnet.module.sd/nan21.dnet.module.sd.ui.extjs/src/main/webapp/src/menu.js
@@ -0,0 +1,22 @@
+Ext.ns('net.nan21.dnet.module.sd.menu');
+var BUNDLE_ID = "nan21.dnet.module.sd.ui.extjs";
+net.nan21.dnet.module.sd.menu.crm = [
+ 
+	{"id":"sm_sd", "text":"Sales & Distribution", leaf:false, children:[
+ 
+	{"id":"sm_sd_order", "text":"Orders", leaf:false, children:[
+		
+		{id:"SalesOrderBaseData_UI", text:Dnet.translate("ui","SalesOrderBaseData_UI"), leaf:true, _frame_:"net.nan21.dnet.module.sd.order.frame.SalesOrderBaseData_UI", _bundle_:BUNDLE_ID},		
+		{id:"SalesOrder_UI", text:Dnet.translate("ui","SalesOrder_UI"), leaf:true, _frame_:"net.nan21.dnet.module.sd.order.frame.SalesOrder_UI", _bundle_:BUNDLE_ID}	]}
+,		 
+	{"id":"sm_sd_invoice", "text":"Invoices", leaf:false, children:[
+		
+		{id:"SalesInvoiceBaseData_UI", text:Dnet.translate("ui","SalesInvoiceBaseData_UI"), leaf:true, _frame_:"net.nan21.dnet.module.sd.invoice.frame.SalesInvoiceBaseData_UI", _bundle_:BUNDLE_ID},		
+		{id:"SalesInvoice_UI", text:Dnet.translate("ui","SalesInvoice_UI"), leaf:true, _frame_:"net.nan21.dnet.module.sd.invoice.frame.SalesInvoice_UI", _bundle_:BUNDLE_ID}	]}
+	]}
+ 
+   ];  
+if (Dnet.navigationTreeMenus.crm == undefined) {
+	Dnet.navigationTreeMenus.crm = { name: "mbMenuCRM" , title :"CRM: Customer Relationships", children: [] };
+}       
+    Dnet.navigationTreeMenus.crm.children = Dnet.navigationTreeMenus.crm.children.concat(net.nan21.dnet.module.sd.menu.crm);
