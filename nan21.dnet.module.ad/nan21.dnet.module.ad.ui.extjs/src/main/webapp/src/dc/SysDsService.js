db41e560eded2a094a19680c7cccf661253c922b
attila.mathe <mathe_attila@yahoo.com>
2012-03-30 10:15:55 +0300

    - taxes menu item

--- nan21.dnet.module.ad/nan21.dnet.module.ad.ui.extjs/src/main/webapp/src/dc/SysDsService.js
+++ nan21.dnet.module.ad/nan21.dnet.module.ad.ui.extjs/src/main/webapp/src/dc/SysDsService.js
@@ -20,6 +20,8 @@ Ext.define("net.nan21.dnet.module.ad.system.dc.SysDsService$CtxList", {
 	extend: "dnet.core.dc.AbstractDcvGrid",
 	alias:"widget.net.nan21.dnet.module.ad.system.dc.SysDsService$CtxList",
 	
+	_noImport_: true,
+	_noExport_: true,
 	_defineColumns_: function () {	
 		this._getBuilder_()	
 		.addTextColumn({ name:"name", dataIndex:"name", width:200 })   	
