fe8364ca3718ddfcaf8df56ea0abaafe7af6a73e
attila.mathe <mathe_attila@yahoo.com>
2012-07-21 07:58:11 +0300

    - restructured db folder structure as db/[install|upgrade]/...  - eclipselink/jpa version by properties in pom files and other details

--- nan21.dnet.module.md/nan21.dnet.module.md.ui.extjs/src/main/webapp/src/dc/FiscalPeriod.js
+++ nan21.dnet.module.md/nan21.dnet.module.md.ui.extjs/src/main/webapp/src/dc/FiscalPeriod.js
@@ -114,6 +114,7 @@ Ext.define("net.nan21.dnet.module.md.base.period.dc.FiscalPeriod$CtxEditList", {
 	extend: "dnet.core.dc.AbstractDcvEditableGrid",
 	alias: "widget.net.nan21.dnet.module.md.base.period.dc.FiscalPeriod$CtxEditList",
 	
+	_bulkEditFields_ : ["type","active","posting"],
 	_defineColumns_: function () {
 		this._getBuilder_()
 		.addTextColumn({ name:"name", dataIndex:"name",width:200,editor:{xtype:"textfield", selectOnFocus:true,allowBlank:false } })
